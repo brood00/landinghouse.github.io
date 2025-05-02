fa05cfedcb6675c26759acc8d8cdbbc90384d62f
Voorhes666 <saninarepe65@gmail.com>
2025-05-03 01:28:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector(".header");
+const burger = header.querySelector(".burger__menu");
+const burgerIcon = header.querySelector(".burger-menu");
+
+
+burger.addEventListener("click", function() {
+    header.classList.toggle("header--mobile");
+
+    if (header.classList.contains("header--mobile")) {
+          burgerIcon.src = "files/burger-close.png"
+    } else {
+        burgerIcon.src = "files/burger-open.png"
+    }
+
+  
+
+})
+
+
+
+
+
+
+
+        
